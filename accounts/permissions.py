eab068afc69d06356f9acc71f6e41347168433d3
ebraheem <ebraheem20012020@gmail.com>
2023-04-15 22:00:02 +0200

    staff is already admin using pre save signal

--- accounts/permissions.py
+++ accounts/permissions.py
@@ -5,10 +5,9 @@ class AdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         return (
             request.user.admin
-            or request.user.is_staff 
             or request.method == 'GET'
         )
 
 class Admin(BasePermission):
     def has_permission(self, request, view):
-        return request.user.admin or request.user.is_staff
+        return request.user.admin
