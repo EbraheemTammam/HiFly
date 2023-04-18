97cbe8a200e4c79fe0a1627efed7f0c42eaf9200
ebraheem <ebraheem20012020@gmail.com>
2023-04-18 14:09:07 +0200

    found out no need for 2 modules

--- fams/settings.py
+++ fams/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 from environs import Env
 
 env = Env()
-env.read_env('dev.env')
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
